98045471a28f57a51185d8323c40d44ee5127343
Miguel Carvalho <73550106+migueltc13@users.noreply.github.com>
2023-01-19 17:12:43 +0000

    Create get_flag.sh

---
+++ write-ups/level1/Welcome/get_flag.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script is a simple way to access a website and retrieve the flag text shown on the webpage.
+
+# Usage: ./script.sh MACHINE_IP
+# Example: ./script.sh 10.10.63.99
+
+# Retrieve the webpage using curl and store the output in a variable
+WEBPAGE=$(curl http://$1)
+
+# Use grep to search for the flag text in the webpage
+FLAG=$(echo $WEBPAGE | grep -o "flag{.*}")
+
+# Print the flag text
+echo "$FLAG"
